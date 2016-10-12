2670b4b241fe13e92730eae008ce295437d7e56f
Biniam Asnake Kefale <biniamasnake@gmail.com>
2016-10-12 08:15:27 +0200

    working project + .gitignore

---
+++ src/main/java/com/hasset/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.hasset;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * @author biniamasnake on 12/10/2016.
+ */
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
---
+++ src/main/java/com/hasset/SbDataRestExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hasset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDataRestExampleApplication.class, args);
+	}
+}
